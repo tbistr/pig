fd17573655109464185fd80103f7b8faf4e85cb9
tbistr <tbistr99@gmail.com>
2023-03-04 02:25:53 +0900

    mv: Find, Text -> `pig.go`

--- pig.go
+++ pig.go
@@ -22,3 +22,36 @@ func ParseS(s string) (Node, error) {
 func ParseB(b []byte) (Node, error) {
 	return Parse(bytes.NewReader(b))
 }
+
+func (n Node) Find(m Match) Node {
+	var inner func(Node)
+	var found Node
+	inner = func(n Node) {
+		if m(n) {
+			found.AppendChild(n)
+			return
+		}
+		for c := n.FirstChild(); c.Node != nil; c = c.NextSibling() {
+			inner(c)
+		}
+	}
+
+	inner(n)
+	return found
+}
+
+func (n Node) Text() string {
+	var t string
+	var inner func(n Node)
+	inner = func(n Node) {
+		if n.Type == html.TextNode {
+			t += n.Data
+		}
+		for c := n.FirstChild(); c.Node != nil; c = c.NextSibling() {
+			inner(c)
+		}
+	}
+
+	inner(n)
+	return t
+}
